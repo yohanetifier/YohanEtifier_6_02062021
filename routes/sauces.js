48e482e99e5b66adaef751825e321ea2cb54f85f
Yohan Etifier <etifier.y@gmail.com>
2021-06-09 07:46:55 +0200

    configuration de multer

--- routes/sauces.js
+++ routes/sauces.js
@@ -1,14 +1,15 @@
 const express = require('express'); 
 const router = express.Router(); 
 const Sauces = require('../models/Sauces'); 
+const sauceCtrl = require('../controllers/sauces'); 
+const auth = require('../middleware/auth'); 
+const multer = require('../middleware/mutler-config'); 
 
-const sauceCtrl = require('../controllers/sauces')
-
-router.post('/', sauceCtrl.createSauce)
-router.put('/:id', sauceCtrl.modifySauce);
-router.delete('/:id', sauceCtrl.deleteSauce)
-router.get('/', sauceCtrl.getAllSauce)
-router.get('/:id', sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce)
+router.put('/:id', auth, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce)
+router.get('/', auth, sauceCtrl.getAllSauce)
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 
 module.exports = router; 
 
